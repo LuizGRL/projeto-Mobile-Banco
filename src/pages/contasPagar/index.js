34ff752bc73cdd6dda2d000837a099d762d303db
LuizGRL <luizguilherme.lgrl@gmail.com>
2022-03-27 18:40:17 -0300

    home

---
+++ src/pages/contasPagar/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+const App = () =>{
+    return(
+        <h1>Contaas a Pagar</h1>
+    );
+}
+export default App;
---
+++ src/pages/contasReceber/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+const App = () =>{
+    return(
+        <h1> Contas a Receber</h1>
+    );
+}
+export default App;
