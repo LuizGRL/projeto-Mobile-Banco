f8b076759e1af3bdbe6b9af56bb5690ed618e587
LuizGRL <luizguilherme.lgrl@gmail.com>
2022-04-03 01:26:01 -0300

    colocando data

--- src/pages/extrato/index.js
+++ src/pages/extrato/index.js
@@ -4,23 +4,28 @@ import Logo from "../../img/lontraLogo2.png"
 import {Link} from "react-router-dom"
 
 const App = () => {
+    const dataHj = new Date()
     const [contas, setValor] = useState([{
         id: "1",
         valorExtrato: "1.299",
-        descricaoExtrato: " Conta de água"
+        descricaoExtrato: " Conta de água",
+        data: dataHj.toLocaleDateString()
     }, {
         id: "2",
         valorExtrato: "300",
-        descricaoExtrato: " Conta de luz"
+        descricaoExtrato: " Conta de luz",
+        data: dataHj.toLocaleDateString()
+
     }, {
         id: "3",
         valorExtrato: "9,90",
-        descricaoExtrato: " Amazon prime"
+        descricaoExtrato: " Amazon prime",
+        data: dataHj.toLocaleDateString()
+
     }]);
     const [saldo,setSaldo] =useState("2.000")
     return (
         <div className="container">
-
             <div className="container-extrato">
                 <div className="wrap-extrato">
                     <span className="extrato-title">
@@ -34,7 +39,8 @@ const App = () => {
                         <div className="historico-extrato-">
                             {contas.map(conta => <div className="historico-extrato-valores">  <p>ID: {conta.id}</p>
                                 <p>Valor: {conta.valorExtrato} R$</p>
-                                <p>Descrição: {conta.descricaoExtrato}</p></div>)}
+                                <p>Descrição: {conta.descricaoExtrato}</p>
+                                <p>Data: {conta.data}</p></div>)}
                         </div>
                     </div>
                     <div className="container-button-0-extrato">
